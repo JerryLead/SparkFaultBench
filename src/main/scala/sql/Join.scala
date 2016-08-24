bbcf610b8c58cd338570c8ff1e160f103f1c61e9
201528015029017 <276627327@qq.com>
2016-08-24 13:53:08 +0800

    refactor add JOIN function

--- src/main/scala/sql/Join.scala
+++ src/main/scala/sql/Join.scala
@@ -2,36 +2,37 @@ package sql
 
 import org.apache.spark.sql.SparkSession
 import sql.BaseUtils._
-/**
-  * Created by lenovo on 2016/8/23 0023.
-  */
 
-object Scan {
+/**
+  * Created by lenovo on 2016/8/24 0024.
+  */
+object Join {
 
 
   def main(args: Array[String]): Unit = {
-
     val warehouseLocation = System.getProperty("user.dir")
     val spark = SparkSession.builder()
-      .appName("Scan")
-      .config("spark.some.config.option", "some-value")
+      .appName("Join")
       .config("spark.sql.warehouse.dir",warehouseLocation)
-      .master("local[2]").getOrCreate()
-    doScanSQL(spark)
+      .master("local[2]")
+      .getOrCreate()
+
+    doJoinSQL(spark)
   }
-  private def doScanSQL(spark:SparkSession):Unit={
+
+  def doJoinSQL(spark:SparkSession) : Unit={
     import spark.implicits._
     val rankingsDF = getRankingsDF(spark)
+    val uservisitsDF = getUservisitsDF(spark)
 
     rankingsDF.createOrReplaceTempView("rankings")
-    val scanDF = spark.sql("SELECT * From rankings where pagerank > 20")
-    scanDF.show()
-
-    val uservisitsDF =getUservisitsDF(spark)
-
     uservisitsDF.createOrReplaceTempView("uservisits")
-    val scanDF2 = spark.sql("SELECT * from uservisits")
-    scanDF2.show()
+    val sqltext = ""
+    val joinDF = spark.sql("select sourceipaddr,url,adrevenue from rankings INNER JOIN uservisits ON url=destinationURL")
+    joinDF.show()
+    //print(joinDF.count())
+
   }
 
 }
+
